06f6b7c818ba2712c90795add1fb5e5df4f4def1
zeynelerenkinali <zeynel.eren.kinali55@erzurum.edu.tr>
2024-10-28 21:28:29 +0300

    take_off location

--- take_off_location.py
+++ take_off_location.py
@@ -1,4 +1,4 @@
-from dronekit import connect, VehicleMode
+from dronekit import connect, VehicleMode , LocationGlobalRelative
 import time
 
 uav = connect('127.0.0.1:14550', wait_ready=True)
@@ -22,15 +22,23 @@ def takeoff(altitude):
         time.sleep(1)
     print("UAV is armable.")
     uav.mode = VehicleMode("GUIDED")
-    time.sleep(1)
-    print(f"UAV mode: {str(uav.mode)}")
+    #print(f"UAV mode: {str(uav.mode)}")
+    uav.armed = True
 
     while uav.armed is not True:
         print("UAV is arming...")
-        uav.armed = True
-        time.sleep(1)
+        time.sleep(0.5)
     
     print(f"UAV armed: {uav.armed}")
     uav.simple_takeoff(altitude)
+    while uav.location.global_relative_frame.alt < altitude * 0.9:
+        print("UAV is taking off...")
+        time.sleep(0.5)
+# You shouldn't put lots of time.sleep() function between arm function and takeoff
+# Because uav is arming itself at so close time and disarming as well in a so close time
+# And then trying to takeoff at the disarmed position.
 
-takeoff(20)
+takeoff(10)
+print("Position is successfully accepted")
+position = LocationGlobalRelative(-35.36227792, 149.16508368, 30)
+uav.simple_goto(position)
